1bdc8ce9bfee5618586e3aae171a752dd94faa9a
d0ndar <popov.m14a@gmail.com>
2025-12-10 17:50:18 +0500

    renamed function in test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_add_to_cart_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
